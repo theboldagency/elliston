ab1fc1f2b1452a3c78812de4b62ed6a038644890
Rob Clark <rob@theboldagency.co>
2021-11-03 19:23:11 -0500

    v1.0.0

--- elliston.php
+++ elliston.php
@@ -3,14 +3,14 @@
  * Plugin Name: Elliston
  * Plugin URI:
  * Description: Bold's in-house brew of custom editor styles and enhancements.
- * Version: 1.0.0-beta1
+ * Version: 1.0.0
  * Author: The Bold Agency
  * Author URI: https://theboldagency.co
  */
 
 function elliston_admin_scripts() {
     $wp_scripts = wp_scripts();
-    wp_enqueue_style( 'elliston', plugin_dir_url( __FILE__ ) . 'assets/css/admin-styles.css', '', '1.0.0-beta1' );
+    wp_enqueue_style( 'elliston', plugin_dir_url( __FILE__ ) . 'assets/css/admin-styles.css', '', '1.0.0' );
 }
 
 add_action( 'admin_enqueue_scripts', 'elliston_admin_scripts' );
